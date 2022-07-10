8606186452f390317d008d8f193307631bceb35a
saul715 <apoc715@yahoo.com>
2022-07-10 13:33:29 -0500

    segunda practica

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,61 @@
 package main.java;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hola mundo");
+        cuartoCodigo();
+    }
+
+    public static void primerCodigo() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("ingrese su valor\n");
+        String entrada = scanner.nextLine();
+        System.out.println(entrada);
+    }
+
+    public static void segundoCodigo() {
+        boolean miguelAngel;
+        miguelAngel = true;
+        if (miguelAngel) {
+            System.out.println("se salvo");
+        } else {
+            System.out.println("se la gano");
+        }
+    }
+
+    public static void tercerCodigo() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("ingrese su valor\n");
+        String entrada = scanner.nextLine();
+
+        if (entrada.equals("1")) {
+            System.out.println("es uno");
+        }
+        else if (entrada.equals("2")) {
+            System.out.println("es dos");
+        }
+        else {
+            System.out.println("no es ni uno ni es dos");
+        }
+    }
+    public static void cuartoCodigo() {
+        List <String> listaPersonas=new ArrayList<>();
+        listaPersonas.add("saul");
+        listaPersonas.add("migue");
+        listaPersonas.add("isa");
+        //System.out.println(listaPersonas);
+        for (int i = 0; i < listaPersonas.size(); i++) {
+            //System.out.println(listaPersonas.get(i));
+            if (listaPersonas.get(i).equals("migue")){
+                System.out.println(listaPersonas.get(i));
+            }
+
+        }
     }
 }
+
+
+
